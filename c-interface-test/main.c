c812fe91f02b1fe9e93f75c3b1b787c9604ad2f8
Johannes Spinneken <johannes@evergreeninnovations.co>
2021-01-22 12:19:27 -0500

    C Interface code

--- c-interface-test/main.c
+++ c-interface-test/main.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "string.h"
 
 // this file creates a simple exe on the cRIO to test the LabVIEW interface lib
 // to compile, run:
@@ -7,18 +8,29 @@
 // make sure libs can be found: export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
 // interface into lib for LabVIEW.
-extern int callROSCO(float *inarr, int *aviFAIL, char *inputStr);
+extern int callROSCO(float *inarr, int *aviFAIL, char *inputStr, char *infileName, char *outfileName);
+
+#define SWAP_SIZE 200
+#define MSG_SIZE 65536
+
+#define C2F 1 // Arrays in C are zero based, FORTRAN is 1 based.
+#define INDEX_DT 3 - C2F
 
 int main(void)
 {
-    float swap[200] = {};
-    int fail;
-    char msg[16384];
+    float swap[SWAP_SIZE] = {};
+    char msg[MSG_SIZE];
     int ret;
+    int fail;
+
+    char infileName[] = "/C/rosco-data/DISCON-UMaineSemi.IN";
+    char outfileName[] = "/C/rosco-data/SimOut.txt";
+
+    swap[INDEX_DT] = 0.001; // set this to non-zero (ROSCO fails otherwise)
 
     printf("Calling ROSCO C-Interface ...\n");
 
-    ret = callROSCO(swap, &fail, msg);
+    ret = callROSCO(swap, &fail, msg, infileName, outfileName);
 
     printf("Finished calling ROSCO C-Interface. Avi fail = %i, msg = %s. Ret = %i\n", fail, msg, ret);
 
---
+++ c-interface/callROSCO.c
@@ -0,0 +1,40 @@
+#include "stdio.h"
+#include "string.h"
+
+// to compile:
+// gcc -fPIC -shared -L/usr/local/lib/libROSCO -o libCallROSCO.so callROSCO.c -lROSCO
+// mv libCallROSCO.so /usr/local/lib
+
+#define MSG_SIZE 16384
+
+#define C2F 1 // Arrays in C are zero based, FORTRAN is 1 based.
+#define INDEX_IN_FILE_LEN 50 - C2F
+#define INDEX_OUT_FILE_LEN 51 - C2F
+#define INDEX_AVCMSG_SIZE 49 - C2F
+
+// interface into the FORTRAN compiled .so file
+extern void DISCON(float *avrSWAP, int *aviFAIL, char *accINFILE, char *avcOUTNAME, char *avcMSG);
+
+// interface for LabVIEW to call
+int callROSCO(float *swap, int *aviFAIL, char *inputStrHndl, char *infileName, char *outfileName)
+{
+
+	// create mem for return message to LabVIEW
+	char avcMSG[MSG_SIZE];
+
+	// calculate input and output file name length for ROSCO
+	swap[INDEX_IN_FILE_LEN] = (float)strlen(infileName);
+	swap[INDEX_OUT_FILE_LEN] = (float)strlen(outfileName);
+
+	// fix the length of the return message
+	swap[INDEX_AVCMSG_SIZE] = (float)MSG_SIZE;
+
+	// call into ROSCO lib
+	DISCON(swap, aviFAIL, infileName, outfileName, avcMSG);
+
+	// copy the return message string
+	strcpy(inputStrHndl, avcMSG);
+	memset(avcMSG, '\0', MSG_SIZE);
+
+	return 0;
+}
