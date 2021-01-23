bc9f32c1d59c3ee1dbe120a20e709c4f6026c75d
Johannes Spinneken <johannes@evergreeninnovations.co>
2021-01-23 15:28:32 -0500

    Working C/ROSCO interface

--- c-interface-test/main.c
+++ c-interface-test/main.c
@@ -2,44 +2,77 @@
 #include "string.h"
 
 // this file creates a simple exe on the cRIO to test the LabVIEW interface lib
-// to compile, run:
-// gcc -L/usr/local/lib/libCallROSCO -o testCInterface main.c -lCallROSCO
+// to compile on cRIO, run:
+// gcc -L/usr/local/lib -o testCInterface main.c -lCallROSCO
+
+// to compile on MacOS:
+// gcc -L/usr/local/lib -o testCInterface main.c -lCallROSCO
 
 // make sure libs can be found: export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
 // interface into lib for LabVIEW.
-extern int callROSCO(float *inarr, int *aviFAIL, char *inputStr, char *infileName, char *outfileName);
+extern int callROSCO(float *inarr, int *aviFAIL, char *inputStr); //, char *infileName, char *outfileName);
 extern float add(float, float);
 
 #define SWAP_SIZE 200
-#define MSG_SIZE 16384
+#define STEPS 5
+
+#define MSG_SIZE 4096
 
 #define C2F 1 // Arrays in C are zero based, FORTRAN is 1 based.
+#define INDEX_ISTATUS 1 - C2F
+#define INDEX_TIME 2 - C2F
 #define INDEX_DT 3 - C2F
+#define INDEX_AVCMSG_SIZE 49 - C2F
+#define INDEX_NUM_BL 61 - C2F
 
 int main(void)
 {
-    float swap[SWAP_SIZE] = {};
-    char msg[MSG_SIZE];
+
     int ret;
-    int fail;
+    int fail, iStatus;
     float a, b, sum;
 
+    float dt = 0.1;
     a = 1.0;
     b = 2.0;
+    fail = -100;
 
     char infileName[] = "/C/rosco-data/DISCON-UMaineSemi.IN";
     char outfileName[] = "/C/rosco-data/SimOut.txt";
 
-    swap[INDEX_DT] = 0.001; // set this to non-zero (ROSCO fails otherwise)
+    //char infileName[] = "/Users/jspinnek/rosco-data/DISCON-UMaineSemi.IN";
+    //char outfileName[] = "/Users/jspinnek/rosco-data/SimOut.txt";
 
     printf("Calling test function add\n");
     sum = add(a, b);
     printf("Result of %f + %f = %f \n\n", a, b, sum);
 
-    printf("Calling ROSCO C-Interface ...\n");
-    ret = callROSCO(swap, &fail, msg, infileName, outfileName);
-    printf("Finished calling ROSCO C-Interface. Avi fail = %i, msg = %s. Ret = %i\n", fail, msg, ret);
+    // fix the length of the return message
+    float swap[SWAP_SIZE] = {};
+
+    swap[INDEX_AVCMSG_SIZE] = (float)MSG_SIZE;
+    swap[INDEX_NUM_BL] = 3;
+
+    for (int i = 0; i <= STEPS; i++)
+    {
+        char msg[MSG_SIZE];
+        if (i == 0)
+        {
+            iStatus = 0;
+        }
+        else
+        {
+            iStatus = 1;
+        }
+
+        swap[INDEX_ISTATUS] = iStatus;
+        swap[INDEX_TIME] = (float)i * dt;
+        swap[INDEX_DT] = dt;
+        printf("Calling ROSCO C-Interface step %u of %u ...\n", i, STEPS);
+        ret = callROSCO(swap, &fail, msg);
+        printf("Finished calling ROSCO C-Interface. Avi fail = %i, msg = %s. Ret = %i\n\n", fail, msg, ret);
+    }
 
     return 0;
 }
--- c-interface/callROSCO.c
+++ c-interface/callROSCO.c
@@ -1,41 +1,64 @@
 #include "stdio.h"
 #include "string.h"
+#include "stdlib.h"
 
-// to compile:
-// gcc -fPIC -shared -L/usr/local/lib/libROSCO -o libCallROSCO.so callROSCO.c -lROSCO
+// to compile on cRIO:
+// gcc -Wall -fmessage-length=0 -fPIC -shared -L/usr/local/lib -o libCallROSCO.so callROSCO.c -lROSCO
 // mv libCallROSCO.so /usr/local/lib
 
-#define MSG_SIZE 16384
+// to compile on MacOS:
+// gcc -fPIC -shared -L/usr/local/lib -o libCallROSCO.so callROSCO.c -ldiscon
+// sudo mv libCallROSCO.so /usr/local/lib
 
 #define C2F 1 // Arrays in C are zero based, FORTRAN is 1 based.
+#define INDEX_TIME 2 - C2F
 #define INDEX_IN_FILE_LEN 50 - C2F
 #define INDEX_OUT_FILE_LEN 51 - C2F
 #define INDEX_AVCMSG_SIZE 49 - C2F
+#define INDEX_NUM_BL 61 - C2F
+
+#define MSG_SIZE 4096
+#define BUF_SIZE 8192
 
 // interface into the FORTRAN compiled .so file
 extern void DISCON(float *avrSWAP, int *aviFAIL, char *accINFILE, char *avcOUTNAME, char *avcMSG);
-extern float add(float a, float b);
 
 // interface for LabVIEW to call
-int callROSCO(float *swap, int *aviFAIL, char *inputStrHndl, char *infileName, char *outfileName)
-{
 
-	// create mem for return message to LabVIEW
-	char avcMSG[MSG_SIZE];
+char msg[MSG_SIZE]; // one extra for the C null char
+char buf[BUF_SIZE];
+
+int callROSCO(float *swap, int *aviFAIL, char *msgIn)
+{
+	char infileName[] = "/C/rosco-data/DISCON-UMaineSemi.IN";
+	char outfileName[] = "/C/rosco-data/SimOut.txt";
+
+	float time;
 
 	// calculate input and output file name length for ROSCO
 	swap[INDEX_IN_FILE_LEN] = (float)strlen(infileName);
 	swap[INDEX_OUT_FILE_LEN] = (float)strlen(outfileName);
-
-	// fix the length of the return message
 	swap[INDEX_AVCMSG_SIZE] = (float)MSG_SIZE;
+	swap[INDEX_NUM_BL] = 3;
 
 	// call into ROSCO lib
-	DISCON(swap, aviFAIL, infileName, outfileName, avcMSG);
+	DISCON(swap, aviFAIL, infileName, outfileName, msg);
 
-	// copy the return message string
-	strcpy(inputStrHndl, avcMSG);
-	memset(avcMSG, '\0', MSG_SIZE);
+	time = swap[INDEX_TIME];
+
+	if (*aviFAIL != 0)
+	{
+		sprintf(buf, "LabVIEW RT to ROSCO interface v0.30.\n\nReceived at total of %i chars from ROSCO.\n\n AVIFail = %i\n\nContent %s", (int)strlen(msg), *aviFAIL, msg);
+	}
+	else
+	{
+		sprintf(buf, "LabVIEW RT to ROSCO interface v0.30.\n\nSuccesfully called interface at t = %f", time);
+	}
+
+	strcpy(msgIn, buf);
+
+	memset(msg, '\0', MSG_SIZE);
+	memset(buf, '\0', BUF_SIZE);
 
 	return 0;
 }
