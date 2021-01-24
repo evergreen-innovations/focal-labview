bc5beead0a5b47cb9475a299862f0ddd70a11181
Johannes Spinneken <johannes@evergreeninnovations.co>
2021-01-24 11:19:14 -0500

    ROSCO src code and compile script

---
+++ rosco-egi-2021-01-24/build/compile-fortran.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Compile rosco code
+
+if ["$1" == ""]; then
+  echo "enter the so name"
+fi
+
+gfortran -fdefault-real-8 -fdefault-double-8 -ffree-form -fPIC -DIMPLICIT_DLLEXPORT -ffree-line-length-0 -c ../src/Constants.f90 
+gfortran -fdefault-real-8 -fdefault-double-8 -ffree-form -fPIC -DIMPLICIT_DLLEXPORT -ffree-line-length-0 -c ../src/ROSCO_Types.f90 
+gfortran -fdefault-real-8 -fdefault-double-8 -ffree-form -fPIC -DIMPLICIT_DLLEXPORT -ffree-line-length-0 -c ../src/Filters.f90 
+gfortran -fdefault-real-8 -fdefault-double-8 -ffree-form -fPIC -DIMPLICIT_DLLEXPORT -ffree-line-length-0 -c ../src/Functions.f90
+gfortran -fdefault-real-8 -fdefault-double-8 -ffree-form -fPIC -DIMPLICIT_DLLEXPORT -ffree-line-length-0 -c ../src/ControllerBlocks.f90 
+gfortran -fdefault-real-8 -fdefault-double-8 -ffree-form -fPIC -DIMPLICIT_DLLEXPORT -ffree-line-length-0 -c ../src/ReadSetParameters.f90 
+gfortran -fdefault-real-8 -fdefault-double-8 -ffree-form -fPIC -DIMPLICIT_DLLEXPORT -ffree-line-length-0 -c ../src/Controllers.f90 
+gfortran -fdefault-real-8 -fdefault-double-8 -ffree-form -fPIC -DIMPLICIT_DLLEXPORT -ffree-line-length-0 -c ../src/DISCON.F90 
+
+
+gfortran -shared Constants.o ControllerBlocks.o Controllers.o DISCON.o ROSCO_Types.o Filters.o Functions.o ReadSetParameters.o -o "$1".so
+echo "Done-Check for .so"
